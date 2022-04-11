345263c3c96d0c376ced06cab5afee32697ed66d
genugx_x <qparanoya@gmail.com>
2022-04-11 21:10:08 +0900

    Go 문법 연습

---
+++ basic/variable.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	var i, j, k int
+	var a, b, c int = 1, 2, 3
+	println(i + j + k)
+	println(a, b, c)
+
+	const d int = 10
+	const e string = "Hi"
+
+	/*
+		[스코프] [변수명] [자료형]
+	*/
+
+}
